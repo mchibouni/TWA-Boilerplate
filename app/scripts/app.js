9d11789da1c917793a58009231531370ea4a20a7
Mehdi <chibouni.mehdi@gmail.com>
2013-10-07 20:59:48 +0100

    whatever works

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -194,7 +194,7 @@ run(function($window, $FB, $rootScope, $location, $anchorScroll, $routeParams) {
       link: 'https://tunisiana.web.awards:9000', // link back to the product or content you are sharing
       picture: 'http://kanalabs.com:9000/images/main-logo.png', // path to an image you would like to share with this content
       caption: 'Phase de soumission : ', // caption
-      description: "J'ai soumis le candidat " + item + "#TWA2013" // description of your product or content
+      description: "Moi j'ai soumis le candidat " +  "#TWA2013" + item  // description of your product or content
     },
     function(response) {
       if (response && response.post_id) {
@@ -202,6 +202,7 @@ run(function($window, $FB, $rootScope, $location, $anchorScroll, $routeParams) {
       } else {
         alert('Post was not published.');
       }
+
     }
     );
     }
